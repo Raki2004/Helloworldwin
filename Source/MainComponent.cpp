939defe4bd23a2542854c2ee0fbf579ef66b2bf8
Ramakrishnan <ramakrishnan0784@gmail.com>
2025-02-14 17:05:09 +0530

    Mindflayer

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,8 +17,8 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::FontOptions (32.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("The Game is On", getLocalBounds(), juce::Justification::centred, true);
+    g.setColour (juce::Colours::red);
+    g.drawText ("Code Red", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
